4aab6a594c1deb20b01c861179180c0b5a0567b1
Christopher Tse <chris@chris-tse.com>
2020-10-18 16:55:40 -0500

    Port old website contents and styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('site/css')
+    eleventyConfig.addPassthroughCopy('site/images')
+    eleventyConfig.addPassthroughCopy('site/scripts')
+
+    eleventyConfig.addNunjucksShortcode("currentYear", function() {
+        let currentDate = new Date()
+
+        return currentDate.getFullYear().toString()
+    });
 
     return {
         dir: { input: 'site', output: 'dist', includes: '_includes' },
